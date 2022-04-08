eab4cf29a35bdd6aaaa3111f65025adf7a3ef9fc
NikitaAnohin03 <101903297+NikitaAnohin03@users.noreply.github.com>
2022-04-08 12:54:50 +0300

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 2; i < 10; i++) {
+            System.out.println("9 x " + i + " = " + 9 * i);
+        }
+    }
+}
